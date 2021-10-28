e0c69ce8d4d3ba39106c583567639237f4190729
Victor Picharelli <picharellivictor@gmail.com>
2021-10-28 09:42:37 -0300

    Projeto Nave DIo

---
+++ src/ConroleAcesso/Program.cs
@@ -0,0 +1,19 @@
+using ConroleAcesso.Forms;
+using System;
+using System.Windows.Forms;
+
+namespace ConroleAcesso
+{
+    static class Program
+    {
+        
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmPrincipal());
+        }
+    }
+}
